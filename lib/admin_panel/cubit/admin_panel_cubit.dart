b207fff8e26bf14f75e871448ae58e9235973175
Alisher <aisroilov1029@gmail.com>
2024-08-09 12:02:33 +0500

    minor improvment

--- lib/admin_panel/cubit/admin_panel_cubit.dart
+++ lib/admin_panel/cubit/admin_panel_cubit.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:meta/meta.dart';
 import 'package:ustudy_app/data/models/course.dart';
 import 'package:ustudy_app/services/ustudy_repository.dart';
 
